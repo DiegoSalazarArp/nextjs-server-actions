bc63afffc7fd2ab856e6e8601e1b31552b6377bf
DiegoSalazar <d.salazararp@gmail.com>
2024-05-25 21:34:01 -0400

    Initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header>
+      <Image src="/vercel.svg" alt="logo" width={100} height={100} />
+    </header>
+  );
+}
